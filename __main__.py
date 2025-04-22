c3aa224f6560b88a996076db0ab56750ead13a75
anirudh singh <anirudhsingh1983@gmail.com>
2025-04-22 17:08:19 -0400

    Ongoing coding.

---
+++ __main__.py
@@ -0,0 +1,43 @@
+import importlib
+import logging
+from datetime import datetime
+
+import pandas as pd
+
+import experiment_settings
+from optimization.optimizer import Optimizer
+from util import utility_functions as uf
+
+pd.set_option('display.expand_frame_repr', True)
+pd.set_option('display.max_columns', None)  # Set to None for unlimited number of output rows.
+pd.set_option('display.max_colwidth', None)
+pd.set_option('display.max_info_columns', 500)
+pd.set_option('display.max_rows', None)  # Set to None for unlimited number of output rows.
+pd.set_option('display.width', 500)  # Width of the display in characters.
+
+experiment_constants = importlib.import_module(
+    f"experiment_configs.{experiment_settings.EXPERIMENT_CONSTANTS_MODULE_NAME}")
+
+
+def main():
+    # begin experiment
+    start = datetime.now()
+    log = logging.getLogger(__name__)
+
+    log.info(f"Backtesting started at: {str(start)}")
+
+    experiment_id = 'e00eb1e7-cc1b-417a-87fe-a8dfff2a2c29'
+    # experiment_id = experiment_constants.EXPERIMENT_ID
+
+    op = Optimizer(experiment_id=experiment_id)
+    best_pipeline = op.execute_optimization(direction='minimize', n_trials=200)
+    print(best_pipeline)
+    uf.save_data(best_pipeline, experiment_id, name=f"final_result")
+
+    # import constants
+    # from util import utility_functions as uf
+    # model_performances = uf.read_data(experiment_id, name=f"{constants.MODELING_NAME}_{constants.MODEL_PERFORMANCE_RESULT_NAME}")
+
+
+if __name__ == "__main__":
+    main()
